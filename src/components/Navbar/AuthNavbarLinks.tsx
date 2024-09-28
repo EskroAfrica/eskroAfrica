ed97e2827052563ce4e3696a0a067bd4e7c299e7
Shobande James Oluwaseun <shobandejames@gmail.com>
2024-09-28 15:12:26 +0100

    changes

--- src/components/Navbar/AuthNavbarLinks.tsx
+++ src/components/Navbar/AuthNavbarLinks.tsx
@@ -3,10 +3,10 @@ import esk from '../../assets/Frame7.png'
 import { useSelector } from 'react-redux';
 import { selectCurrentUser } from '../../store/userDetailsSlice';
 import { Link } from 'react-router-dom';
-import { FaBell } from "react-icons/fa";
+import { FaBell, FaUserCircle } from "react-icons/fa";
 import { RiHandHeartFill } from "react-icons/ri";
-import { FaCartShopping } from "react-icons/fa6";
-import { FaUserCircle } from "react-icons/fa";
+import { FaCartShopping, FaSquarePlus } from "react-icons/fa6";
+import { IoIosArrowDown } from "react-icons/io";
 
 const AuthNavbarLinks = () => {
   const user = useSelector(selectCurrentUser);
@@ -26,9 +26,9 @@ const AuthNavbarLinks = () => {
           <p className="text-3xl text-primary"><FaCartShopping /></p>
           <div className='flex items-center rounded-full bg-neutral-100 px-4 py-3 border border-neutral-300'>
             <p className='text-3xl pe-3 text-neutral-500'><FaUserCircle /></p>
-            <p className='font-normal'>{user}</p>
+            <p className='font-normal flex items-center'>{user} <span className='ps-6'><IoIosArrowDown/></span></p>
           </div>
-          <button className='bg-primary px-5 lg:px-16 py-3 rounded-lg text-white font-normal'>Sell Product</button>
+          <button className='bg-primary px-2 lg:px-12 py-3 rounded-lg text-white font-normal flex items-center'> <span className='text-2xl pe-4'><FaSquarePlus/></span> Sell Product </button>
         </div>
       }
     </>
